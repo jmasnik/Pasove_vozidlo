ab3afb5a3f24eaca6d234ff46e7cd735476d5ca8
Jiri Masnik <jmasnik@gmail.com>
2024-12-29 19:08:43 +0100

    Rozsahy serve, zruseni parsovani UDP

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include <ESPAsyncWebServer.h>
 #include <WiFiUdp.h>
 #include <ESP32Servo.h>
+#include <esp_wifi.h>
+#include <esp_now.h>
+#include <main.h>
 
 // https://gist.github.com/santolucito/4016405f54850f7a216e9e453fe81803
 
@@ -27,8 +30,20 @@
 
 Servo servo;
 
+char mac_address[100];
+
 // AsyncWebServer on port 80
 AsyncWebServer server(80);
+
+typedef struct espnow_message_pasak {
+  uint8_t type;
+  int16_t motor_left;
+  int16_t motor_right;
+  uint8_t led;
+  uint8_t servo;
+} espnow_message_pasak;
+
+espnow_message_pasak message_pasak;
 
 // Websocket
 //AsyncWebSocket ws("/ws");
@@ -45,17 +60,26 @@ int16_t control_left;
 int16_t control_right;
 uint8_t control_servo;
 
+long act_servo;
+
 IPAddress controller_ip;
 uint8_t controller_ip_have;
 
 unsigned long back_link_last_ms;
+unsigned long serial_out_last_ms;
 
 uint16_t tx_cnt;
 
 void initWiFi();
 void initPin();
 void initServer();
+void initESPNow();
+
 void controlToAction();
+void readMacAddress();
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len);
+
 
 ESP32PWM pwm;
 
@@ -67,38 +91,24 @@ void setup() {
 
   controller_ip_have = 0;
   back_link_last_ms = 0;
+  serial_out_last_ms = 0;
 
   tx_cnt = 0;
 
   control_led = 0;
   control_left = 0;
   control_right = 0;
-  control_servo = 0;
+  control_servo = 50;
 
   ESP32PWM::allocateTimer(0);
 
   servo.setPeriodHertz(50);
   servo.attach(PIN_SERVO);
-  
-/*
-while(1){
-    for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
-        servo.write(posDegrees);
-        Serial.println(posDegrees);
-        delay(20);
-    }
-
-    for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
-        servo.write(posDegrees);
-        Serial.println(posDegrees);
-        delay(20);
-    }
-}
-*/
 
   initPin();
   initWiFi();
-  initServer();
+  //initServer();
+  initESPNow();
 
   ledcWrite(PIN_PWM_CH1, 0);
   ledcWrite(PIN_PWM_CH2, 0);
@@ -154,8 +164,9 @@ void controlToAction(){
     ledcWrite(PIN_PWM_CH2, -control_right);
   }  
 
-  // Servo
-  servo.write(map(control_servo, 0, 100, 0, 180));
+  // Servo (aktualne rozsah 50 - 110 deg)
+  act_servo = map(control_servo, 0, 100, 50, 110);
+  servo.write(act_servo);
 
 }
 
@@ -163,16 +174,17 @@ void controlToAction(){
  * Hlavni
  */
 void loop() {
-    uint8_t i;
-    char *ch_s;
-    char *ch_pt1;
-    char *ch_pt2;
-    char str[20];
+    //uint8_t i;
+    //char *ch_s;
+    //char *ch_pt1;
+    //char *ch_pt2;
+    //char str[20];
     char buff[100];
     unsigned long ms;
 
     ms = millis();
 
+    /*
     int packetSize = Udp.parsePacket();
 
     if (packetSize){
@@ -193,7 +205,6 @@ void loop() {
             ch_pt2 = strchr(ch_pt1 + 1, '|');
             memcpy(str, ch_pt1 + 1, ch_pt2 - ch_pt1 - 1);
             str[ch_pt2 - ch_pt1 - 1] = '\0';
-            //Serial.println(str);
             ch_s = ch_pt2;
 
             if(i == 0) control_left = -atoi(str);
@@ -204,13 +215,12 @@ void loop() {
         }
 
         Serial.printf("UDP packet contents: %s\n", incomingPacket);
-        
-        sprintf(buff, "Left: %d Right: %d LED: %u Servo: %u", control_left, control_right, control_led, control_servo);
-        Serial.println(buff);
       }
       
     } 
+    */
 
+    // posilani dat z vozidla pres UDP (ted nic, nema IP toho co to ridi)
     if(ms - back_link_last_ms > 1000 && controller_ip_have == 1){
       tx_cnt++;
       sprintf(buff, "Ahoj z vozidla (%u)!", tx_cnt);
@@ -222,23 +232,16 @@ void loop() {
       back_link_last_ms = ms;
     }
 
+    // pravidelny vypis stavu na seriak
+    if(ms - serial_out_last_ms > 500){
+      sprintf(buff, "CLeft: %d CRight: %d CLED: %u CServo: %u AServo: %ld", control_left, control_right, control_led, control_servo, act_servo);
+      Serial.println(buff);
+      
+      serial_out_last_ms = ms;
+    }
+
+    // nastaveni motoru podle ovladacu
     controlToAction();
-
-  /*
-  uint8_t pwm;
-
-  digitalWrite(PIN_LED, HIGH);
-  for(pwm = 0; pwm < 255; pwm++){
-    ledcWrite(PIN_PWM_CH1, pwm);
-    delay(60);
-  }
-
-  digitalWrite(PIN_LED, LOW);
-  for(pwm = 255; pwm > 0; pwm--){
-    ledcWrite(PIN_PWM_CH1, pwm);
-    delay(60);
-  }
-  */
 }
 
 /**
@@ -296,14 +299,88 @@ void initWiFi() {
 
   // STA
   WiFi.mode(WIFI_STA);
+  
+  // Prenastavime kanal
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_channel(10, WIFI_SECOND_CHAN_NONE);
+  esp_wifi_set_promiscuous(false);
+
+
+  /*
   WiFi.begin("mController", "heslo123");
-  Serial.print("Connecting to WiFi ..");
+  Serial.print("Connecting to WiFi mController");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
-    delay(250);
+    delay(200);
   }
   Serial.println();
 
+  // IP
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
+
+  // Gateway IP
+  Serial.print("Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+  */
+
+  // MAC
+  readMacAddress();
+}
+
+/**
+ * Nacte MAC adresu
+ */
+void readMacAddress(){
+  uint8_t baseMac[6];
+  esp_err_t ret;
+  char buff[100];
+
+  strcpy(mac_address, "");
+  
+  ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  if (ret == ESP_OK) {
+    sprintf(mac_address, "%02x %02x %02x %02x %02x %02x", baseMac[0], baseMac[1], baseMac[2],baseMac[3], baseMac[4], baseMac[5]);
+    sprintf(buff, "MAC address: %s", mac_address);
+    Serial.println(buff);
+  } else {
+    Serial.println("Failed to read MAC address");
+  }
+}
+
+/**
+ * Inicializace ESPNow
+ */
+void initESPNow(){
+  // inicializace
+  if (esp_now_init() != ESP_OK) {
+    log_d("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // registrace prijimaciho callbacku
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+}
+
+/**
+ * Prijeti ESPNow
+ */
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  //char buff[200];
+
+  if(len == sizeof(message_pasak) && incomingData[0] == 0x02){
+    memcpy(&message_pasak, incomingData, sizeof(message_pasak));
+
+    //sprintf(buff, "ESPNow / Left: %d  Right: %d  Servo: %u  LED: %u", message_pasak.motor_left, message_pasak.motor_right, message_pasak.servo, message_pasak.led);
+    //Serial.println(buff);
+
+    control_left = message_pasak.motor_left;
+    control_right = message_pasak.motor_right;
+    control_led = message_pasak.led;
+    control_servo = message_pasak.servo;
+
+  } else {
+    Serial.println("Received ESPNow");
+  }
+
 }
